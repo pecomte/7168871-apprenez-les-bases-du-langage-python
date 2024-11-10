a43b93483c0060b6e7126d662a3969899f4ed8c1
pecomte <philippe.comte@icam.fr>
2024-11-10 15:45:35 +0100

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -2,6 +2,28 @@ def main():
     # Ecrivez votre code ici !
     # Attention tout votre code doit être indenté comme ce commentaire
 
+    nombre_a_gauche= input("Entrez un nombre entier : ")
+    nombre_a_droite= input("Entrez un nombre entier : ")
+    opération = input("Entrez l'opération souhaitée ['+', '-', '*' ou '/'] : ")
+resultat="0"
+x = isinstance(nombre_a_gauche,int)
+y = isinstance(nombre_a_droite,int)
+if x=False or y=False:\
+	print("un des facteurs n'est pas entier")
+	exit()
+match symbole:
+	case= "+":
+	case="-":
+	case="/":
+	case="*":
+	case _:
+		print("le symbole n'est pas correct")
+		exit()
+if symbole="/" and nombre_a_droite= 0
+	print("Erreur: impossible de diviser par 0")
+resultat= nombre_gauche symbole nombre_a-droite
+	print(f" Le résultat est : {resultat}")
+
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
