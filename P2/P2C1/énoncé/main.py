bd8ba1fb8bca939b37ea48494b91418d703fce10
pecomte <philippe.comte@icam.fr>
2024-11-10 10:39:05 +0100

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,6 +1,12 @@
 def main():
     # Ecrivez votre code ici !
     # Attention tout votre code doit être indenté comme ce commentaire
+    ensoleille=True
+        if ensoleille:
+            print("on va à la plage")
+        else
+            print("on reste à la maison")
+    
 
 
 # Ne touchez pas le code ci-dessous
